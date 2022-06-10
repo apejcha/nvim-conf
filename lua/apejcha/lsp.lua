ce78d8cf677589da0c815dc0cd617b72203ea36e
Ondřej Lukeš <mail@ondralukes.cz>
2022-06-10 10:02:02 +0200

    Keymaps

--- lua/apejcha/lsp.lua
+++ lua/apejcha/lsp.lua
@@ -14,22 +14,51 @@ lsp_installer.setup({
 local capabilities = vim.lsp.protocol.make_client_capabilities()
 capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-local lspconfig = require("lspconfig")
+local on_attach = function (client,bufnr)
+   local bufopts = {noremap = true,silent = true,buffer=bufnr}
+    vim.keymap.set("n","K", vim.lsp.buf.hover,bufopts)
+    vim.keymap.set("n","gd", vim.lsp.buf.definition,bufopts )
+    vim.keymap.set("n","gt", vim.lsp.buf.type_definition,bufopts )
+    vim.keymap.set("n","gi", vim.lsp.buf.implementation,bufopts)
+end
 
+local lspconfig = require("lspconfig")
+lspconfig.rust_analyzer.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+lspconfig.tsserver.setup{
+  on_attach = on_attach,
+  capabilities = capabilities,
+}
+
+
+lspconfig.clangd.setup{
+  on_attach = on_attach,
+  capabilities = capabilities
+}
+
+lspconfig.ltex.setup{
+  on_attach = on_attach,
+  capabilities = capabilities
+}
 
 lspconfig.sumneko_lua.setup{
-  opts ={
+  on_attach = on_attach,
+  capabilities = capabilities,
   settings = {
     Lua = {
       diagnostics = {
-        globals = {'vim'},
-      }
+        global = {'vim'},
+      },
+      workspace = {
+				-- Make the server aware of Neovim runtime files
+				library = {
+					[vim.fn.expand("$VIMRUNTIME/lua")] = true,
+					[vim.fn.expand("$VIMRUNTIME/lua/vim/lsp")] = true,
+				},
+			},
     }
   }
 }
-}
-
-lspconfig.rust_analyzer.setup{}
-lspconfig.tsserver.setup{}
-lspconfig.ltex.setup{}
-lspconfig.clangd.setup{}
